524de63311b2f10b9833de7490a9a367b4a592a9
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-14 00:28:37 +0100

    boil boil boil

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './style.scss';
+const imagePath = require('./resources/logo.png');
+const buffer = require('arraybuffer-loader!./resources/logo.png');
+const PNG = require('pngjs').PNG;
+
+console.dir(buffer);
+//const array = new Uint8Array(buffer);
+console.dir(imagePath);
 
 var canvas = document.getElementById("raster");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const imageData = ctx.createImageData(320, 240);
+
+const img = new Image(320, 240);
+img.src = imagePath.default;
+img.onload = function() {
+    imageData.drawImage(img, 0, 0);
+    //ctx.drawImage(img, 0, 0);
+
+};
