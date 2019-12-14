a3149ee147c83b3abdbc757623ffd338d4f50a4d
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-14 01:08:23 +0100

    boil boil boil

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import './style.scss';
-const imagePath = require('./resources/logo.png');
-const buffer = require('arraybuffer-loader!./resources/logo.png');
-const PNG = require('pngjs').PNG;
 
-console.dir(buffer);
-//const array = new Uint8Array(buffer);
-console.dir(imagePath);
+import buffer from './resources/logo.png';
+const canvas = document.getElementById("raster");
+const ctx = canvas.getContext("2d");
 
-var canvas = document.getElementById("raster");
-var ctx = canvas.getContext("2d");
-const imageData = ctx.createImageData(320, 240);
-
-const img = new Image(320, 240);
-img.src = imagePath.default;
-img.onload = function() {
-    imageData.drawImage(img, 0, 0);
-    //ctx.drawImage(img, 0, 0);
-
+const image = new Image(520, 256);
+image.src = buffer;
+image.onload = function() {
+    ctx.drawImage(image, 0, 0);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -42,10 +42,10 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
-                        loader: 'file-loader',
-                        options: {
+                        loader: 'url-loader',
+                        /*options: {
                             name: '[path][name].[ext]',
-                        },
+                        },*/
                     },
                 ],
             },
